4f40a47341dd6ac62b2806ad916245912813570e
MihalisP <mihalispapak@gmail.com>
2020-12-01 20:05:54 +0200

    minor updts

--- db/oasa_pull.py
+++ db/oasa_pull.py
@@ -1,5 +1,11 @@
+import sqlite3
+
 import requests
+import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from db.app import app
 from db.models import Bus, Stop, Association
 
@@ -14,16 +20,17 @@ db = SQLAlchemy(app)
 def fill_stops():
     for resp in json_response:
         # Take the routecode, need linecode for that
-        routecodeResponse = requests.post("http://telematics.oasa.gr/api/?act=webGetRoutes&p1=" + resp['LineCode']).json()
+        routecodeResponse = requests.post(
+            "http://telematics.oasa.gr/api/?act=webGetRoutes&p1=" + resp['LineCode']).json()
         routecode = routecodeResponse[0]['RouteCode']
 
         # take stop codes and names, need routecode for that. One to many
         responseStopCode = requests.post("http://telematics.oasa.gr/api/?act=webGetStops&p1=" + str(routecode)).json()
         for sc in responseStopCode:
-            stop_codes = (sc['StopCode'])   # each stop has a stop code
+            stop_codes = (sc['StopCode'])  # each stop has a stop code
             stop_names = (sc['StopDescr'])  # for each stop code we have a stop name
 
-            record = Stop(route_code=routecode, stop_codes=stop_codes, stop_names=stop_names)   # change to bulk insert
+            record = Stop(route_code=routecode, stop_codes=stop_codes, stop_names=stop_names)  # change to bulk insert
             db.session.add(record)
             db.session.commit()
 
@@ -36,9 +43,25 @@ def fill_bus():
             "http://telematics.oasa.gr/api/?act=webGetRoutes&p1=" + resp['LineCode']).json()
         routecode = routecodeResponse[0]['RouteCode']
 
-        record = Bus(line_code=resp['LineCode'], line_id=resp['LineID'], line_descr=resp['LineDescr'], route_code=routecode)
+        record = Bus(line_code=resp['LineCode'], line_id=resp['LineID'], line_descr=resp['LineDescr'],
+                     route_code=routecode)
+
         db.session.add(record)
         db.session.commit()
+
+        fill_associations(record)
+
+
+def fill_associations(bus):
+    [insert_association(bus, stop) for stop in Stop.query.filter_by(route_code=bus.route_code).all()]
+
+
+def insert_association(bus, stop):
+    assoc = Association()
+    assoc.buses_id = bus.id
+    assoc.stops_id = stop.id
+    db.session.add(assoc)
+    db.session.commit()
 
 
 # fill bus and stop tables
