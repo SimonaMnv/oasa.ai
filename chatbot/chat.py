ec112bc07fa8d5a9b720a57259a1dd2ab712cf39
MihalisP <mihalispapak@gmail.com>
2020-12-07 23:06:09 +0200

    line code fix

--- chatbot/chat.py
+++ chatbot/chat.py
@@ -16,7 +16,7 @@ def get_bot_response():
     resp, tag, stop = getResponse(userText)
     if stop is not None:
         stops = [st for st in Stop.query.filter_by(stop_names=stop.stop_names).all()]
-        bus_codes = [b.bus.line_code + ": " + b.bus.line_descr for st in stops for b in st.buses]
+        bus_codes = [b.bus.line_id + ": " + b.bus.line_descr for st in stops for b in st.buses]
         resp = str(resp) + " είναι παιδάκι μου πολλά, να πχ: " + ", ".join(bus_codes)
         resp += ", αλλά μεγάλη γυναίκα είμαι, μπορεί να ξεχνάω τίποτα"
     return str(resp)
